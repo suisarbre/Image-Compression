1c47e3e36aa73376c2151f2ec45bb7e287ef6216
suisarbre <106571710+suisarbre@users.noreply.github.com>
2025-02-04 14:36:29 -0800

    loading the image

---
+++ src/core/imageloader.py
@@ -0,0 +1,33 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_image(path):
+    try:
+        img = Image.open(path)
+        img = img.convert('L')
+        img = np.array(img)
+        plt.imshow(img, cmap='gray')
+        plt.title("Original Grayscale Image")
+        plt.axis('off')
+        plt.show()
+        
+    except FileNotFoundError:
+        print("File not found")
+        return None
+    
+    except Exception as e:
+        print("An error occured: ", e)
+        return None
+    
+    return img
+
+    
+if __name__ == "__main__":
+    image_path = "data/input/nature-7398357_1920.jpg"  # Replace with your image path
+    matrix = load_image(image_path)
+    
+    if matrix is not None:
+        print("Image loaded successfully!")
+        print(f"Image shape (height, width): {matrix.shape}")
+        print(f"Pixel range: {matrix.min()} to {matrix.max()} (0=black, 255=white)")
